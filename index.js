9b41b059c2dc969b2b5e97a3a15e416b4fa2cef3
sunny <indiasuny000@gmail.com>
2015-02-13 23:23:21 +0530

    Added license, imported filepicker and zest runner.

--- index.js
+++ index.js
@@ -2,13 +2,27 @@ var self = require('sdk/self');
 
 const { Sidebar } = require('sdk/ui/sidebar');
 const { Hotkey } = require('sdk/hotkeys');
+var FilePicker = require('filepicker-jetpack'),
+    ZestRunner = require('zest-runner');
 
 let visibility = true;
 
 let sidebar = Sidebar({
   id: 'zestsidebar',
   title: 'Zest',
-  url: self.data.url('data/zest-sidebar/index.html')
+  url: self.data.url('zest-sidebar/index.html'),
+  onAttach: function (worker) {
+    worker.port.on('IMPORT', () => {
+      let fp = new FilePicker({fileExtension: '.zst'});
+      let z = fp.open('data');
+      if (z) {
+        worker.port.emit('RCV-IMPORT', z);
+      }
+      else {
+        console.log('Error: Failed to import file');
+      }
+    })
+  }
 });
 
 sidebar.show();
--- test/test-index.js
+++ test/test-index.js
@@ -2,7 +2,6 @@ var main = require("../");
 
 exports["test main"] = function(assert) {
   assert.pass("Unit test running!");
-  assert.ok((2 === 2), '2 should be equal to 2');
 };
 
 exports["test main async"] = function(assert, done) {
